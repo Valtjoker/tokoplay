26949990d947bb157788973db2d8f7852fc6afdb
valtjoker <adjiugrohoseno@gmail.com>
2023-07-28 12:03:47 +0700

    hotfix

--- server/app.js
+++ server/app.js
@@ -21,9 +21,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/", router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
